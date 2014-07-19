52d5a2062a6843c732b12c07b7954d8c45be221a
JulenFern <julenfern@euskaltel.net>
2014-07-19 12:21:21 +0200

    Initial Commit

---
+++ Ej2T1Julen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ej2T1Julen
+//
+//  Created by Julen on 19/7/14.
+//  Copyright (c) 2014 Julen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ej2T1Julen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ej2T1Julen
+//
+//  Created by Julen on 19/7/14.
+//  Copyright (c) 2014 Julen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
